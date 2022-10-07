f7c6a141f3d2208ef701a41e24c95be13561926f
engrshevie <88986280+engrshevie@users.noreply.github.com>
2022-10-06 21:43:19 -0400

    Create sg.tf

---
+++ sg.tf
@@ -0,0 +1,38 @@
+resource "aws_security_group" "rds" {
+  name   = "education_rds"
+  vpc_id = aws_vpc.vpc_net.id
+
+  ingress {
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "education_rds"
+  }
+}
